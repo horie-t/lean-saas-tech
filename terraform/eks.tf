f345a9740d1ba918636d90498b37d6d513b71091
HORIE Tetsuya <horie-t@users.noreply.github.com>
2025-05-03 20:59:11 +0900

    EKSの構築をネットの記事を参照して行う

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,16 +1,30 @@
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.36"
-
-  cluster_name    = var.cluster_name
-  cluster_version = "1.28"
-
-  cluster_endpoint_public_access = true
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "~> 20.36"
+  cluster_version = "1.32"
+  cluster_name    = "sample-cluster"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
 
-  # Fargate Profiles
+  enable_irsa               = true
+
+  create_cluster_security_group = false
+  create_node_security_group = false
+
+  cluster_addons = {
+    kube-proxy = {}
+    vpc-cni    = {}
+    # ここが大事
+    coredns = {
+      configuration_values = jsonencode({
+        computeType = "Fargate"
+      })
+    }
+  }
+
   fargate_profiles = {
     default = {
       name = "default"
@@ -22,45 +36,11 @@ module "eks" {
           namespace = "kube-system"
         }
       ]
-      subnet_ids = module.vpc.private_subnets
-      tags = {
-        Environment = var.environment
-      }
-    }
-    applications = {
-      name = "applications"
-      selectors = [
-        {
-          namespace = "applications"
-        }
-      ]
-      subnet_ids = module.vpc.private_subnets
-      tags = {
-        Environment = var.environment
-      }
     }
   }
-
-  # Create EKS Managed Node Group(s)
-  eks_managed_node_groups = {}
-
-  # Configure AWS Auth for Fargate
-  enable_cluster_creator_admin_permissions = true
 
   tags = {
     Environment = var.environment
     Terraform   = "true"
   }
 }
-
-# Coredns Fargate configuration
-resource "kubernetes_namespace" "applications" {
-  depends_on = [module.eks]
-  metadata {
-    name = "applications"
-  }
-}
-
-# Configure CoreDNS to run on Fargate
-# This part is typically handled in a post-deployment step via kubectl
-# as part of a complete EKS Fargate deployment
--- terraform/main.tf
+++ terraform/main.tf
@@ -12,15 +12,31 @@ provider "kubernetes" {
   }
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      command     = "aws"
+    }
+  }
+}
+
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.97.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
+      version = "~> 2.36"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.17.0"
     }
   }
   required_version = ">= 1.0.0"
